246197bc2f7d5fe4274d8b7af5426a67950a04a2
Bram Borggreve <borggreve@gmail.com>
2019-07-18 13:44:52 +0200

    Remove schematics

--- schematics/install/files/root/serve-script.js
+++
@@ -1,6 +0,0 @@
-const { LiveReloadCompiler } = require('@nestjs/serve-static');
-
-const compiler = new LiveReloadCompiler({
-  projectName: '<%= getClientProjectName() %>'
-});
-compiler.run();
--- schematics/install/files/root/webpack.server.config.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack');
-const WebpackConfigFactory = require('@nestjs/serve-static')
-  .WebpackConfigFactory;
-
-module.exports = WebpackConfigFactory.create(webpack, {
-  // Nest server for SSR
-  server: './server/main.ts'
-});
