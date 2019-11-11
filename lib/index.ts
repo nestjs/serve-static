811af62d991837527697c8550a3576df4972d63c
Coroliov Oleg <coroliov.o@goparrot.ai>
2019-11-11 15:56:51 +0200

    Remove utils

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,6 @@ export * from './loaders/abstract.loader';
 export * from './loaders/express.loader';
 export * from './loaders/fastify.loader';
 export * from './loaders/noop.loader';
-export * from './utils/service-static.utils';
 export * from './serve-static.constants';
 export * from './serve-static.module';
 export * from './serve-static.providers';
