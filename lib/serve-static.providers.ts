48efd0dcb026967b36f4e6c2d70b692c116efc30
Pedro <pedrogamezcantero@gmail.com>
2021-05-19 11:07:48 +0200

    fix(fastify) New files requires a server restart to be served

--- lib/serve-static.providers.ts
+++ lib/serve-static.providers.ts
@@ -1,9 +1,12 @@
 import { Provider } from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
+import { NotFoundExceptionFilter } from './filters/not-found-excepion.filter';
+import { ServeStaticModuleOptions } from './interfaces/serve-static-options.interface';
 import { AbstractLoader } from './loaders/abstract.loader';
 import { ExpressLoader } from './loaders/express.loader';
 import { FastifyLoader } from './loaders/fastify.loader';
 import { NoopLoader } from './loaders/noop.loader';
+import { SERVE_STATIC_MODULE_OPTIONS } from './serve-static.constants';
 
 export const serveStaticProviders: Provider[] = [
   {
@@ -23,5 +26,13 @@ export const serveStaticProviders: Provider[] = [
       return new ExpressLoader();
     },
     inject: [HttpAdapterHost]
+  },
+  {
+    provide: APP_FILTER,
+    useFactory: (httpAdapterHost: HttpAdapterHost, loader: AbstractLoader, opts: ServeStaticModuleOptions[]) => {
+      if( loader instanceof FastifyLoader )
+        return new NotFoundExceptionFilter(httpAdapterHost, loader, opts);
+    },
+    inject: [HttpAdapterHost, AbstractLoader, SERVE_STATIC_MODULE_OPTIONS]
   }
 ];
