9789ac316336fb2b444bf6813827df90e9a6bb76
Heja <74791506+schart@users.noreply.github.com>
2023-08-06 21:33:51 +0300

    Update fastify.loader.ts
    I was did edit, about of indendation and attributes saves in variable

--- lib/loaders/fastify.loader.ts
+++ lib/loaders/fastify.loader.ts
@@ -1,14 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { loadPackage } from '@nestjs/common/utils/load-package.util';
-import { AbstractHttpAdapter } from '@nestjs/core';
 import * as fs from 'fs';
-import { ServeStaticModuleOptions } from '../interfaces/serve-static-options.interface';
-import {
-  DEFAULT_RENDER_PATH,
-  DEFAULT_ROOT_PATH
-} from '../serve-static.constants';
-import { validatePath } from '../utils/validate-path.util';
+import { Injectable } from '@nestjs/common';
+import { AbstractHttpAdapter } from '@nestjs/core';
 import { AbstractLoader } from './abstract.loader';
+import { validatePath } from '../utils/validate-path.util';
+import { loadPackage } from '@nestjs/common/utils/load-package.util';
+import { DEFAULT_RENDER_PATH, DEFAULT_ROOT_PATH } from '../serve-static.constants';
+import { ServeStaticModuleOptions } from '../interfaces/serve-static-options.interface';
 
 @Injectable()
 export class FastifyLoader extends AbstractLoader {
@@ -31,25 +28,26 @@ export class FastifyLoader extends AbstractLoader {
 
       if (options.serveRoot) {
         app.register(fastifyStatic, {
-          root: clientPath,
-          ...(options.serveStaticOptions || {}),
+          root: clientPath, ...(options.serveStaticOptions || {}),
           wildcard: false,
           prefix: options.serveRoot
         });
 
-        const renderPath =
-          typeof options.serveRoot === 'string'
-            ? options.serveRoot + validatePath(options.renderPath as string)
-            : options.serveRoot;
+       const optionRenderPath =  options.renderPath as string;
+       const optionserveRoot = options.serveRoot;
+        
+       const renderPath = typeof options.serveRoot === 'string'
+        ? optionserveRoot + validatePath(optionRenderPath)
+        : optionserveRoot;
 
         app.get(renderPath, (req: any, res: any) => {
           const stream = fs.createReadStream(indexFilePath);
-          res.type('text/html').send(stream);
+          res.type('text/html')  
+             .send(stream);
         });
       } else {
         app.register(fastifyStatic, {
-          root: clientPath,
-          ...(options.serveStaticOptions || {}),
+          root: clientPath, ...(options.serveStaticOptions || {}),
           wildcard: false
         });
         app.get(options.renderPath, (req: any, res: any) => {
