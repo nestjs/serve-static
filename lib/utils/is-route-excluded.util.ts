a5a6fc9b6836ebf14f2c2c90065cd6212a4d0945
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-20 09:41:27 +0100

    chore: update path-to-regexp

--- lib/utils/is-route-excluded.util.ts
+++ lib/utils/is-route-excluded.util.ts
@@ -1,15 +1,15 @@
-import * as pathToRegexp from 'path-to-regexp';
+import { pathToRegexp } from 'path-to-regexp';
 
 export const isRouteExcluded = (req: any, paths: string[] = []) => {
   return paths.some((path) => {
-    const re = pathToRegexp(path);
+    const { regexp } = pathToRegexp(path);
     const queryParamsIndex = req.originalUrl.indexOf('?');
     const pathname =
       queryParamsIndex >= 0
         ? req.originalUrl.slice(0, queryParamsIndex)
         : req.originalUrl;
 
-    if (!re.exec(pathname + '/')) {
+    if (!regexp.exec(pathname + '/')) {
       return false;
     }
     return true;
