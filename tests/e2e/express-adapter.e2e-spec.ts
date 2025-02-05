nestjs/serve-static

commit 0ae6f5415a81e4ea7daab65650f4015f986afedb

chore: add e2e tests, handle 404 errors, add missing opts

lib/loaders/express.loader.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { loadPackage } from '@nestjs/common/utils/load-package.util';
-import * as fs from 'fs';
 import { AbstractHttpAdapter } from '@nestjs/core';
+import * as fs from 'fs';
 import { ServeStaticModuleOptions } from '../interfaces/serve-static-options.interface';
 import {
-  DEFAULT_RENDER_PATH,
+  DEFAULT_EXPRESS_RENDER_PATH,
   DEFAULT_ROOT_PATH
 } from '../serve-static.constants';
 import { isRouteExcluded } from '../utils/is-route-excluded.util';
@@ -22,20 +22,22 @@ export class ExpressLoader extends AbstractLoader {
       require('express')
     );
     optionsArr.forEach((options) => {
-      options.renderPath = options.renderPath || DEFAULT_RENDER_PATH;
-      const clientPath = options.rootPath || DEFAULT_ROOT_PATH;
+      options.renderPath = options.renderPath ?? DEFAULT_EXPRESS_RENDER_PATH;
+      const clientPath = options.rootPath ?? DEFAULT_ROOT_PATH;
       const indexFilePath = this.getIndexFilePath(clientPath);
 
       const renderFn = (req: unknown, res: any, next: Function) => {
         if (!isRouteExcluded(req, options.exclude)) {
-          if (
-            options.serveStaticOptions &&
-            options.serveStaticOptions.setHeaders
-          ) {
+          if (options.serveStaticOptions?.setHeaders) {
             const stat = fs.statSync(indexFilePath);
             options.serveStaticOptions.setHeaders(res, indexFilePath, stat);
           }
-          res.sendFile(indexFilePath);
+          res.sendFile(indexFilePath, null, (err: Error) => {
+            if (err) {
+              const error = new NotFoundException(err.message);
+              res.status(error.getStatus()).send(error.getResponse());
+            }
+          });
         } else {
           next();
         }
@@ -56,6 +58,15 @@ export class ExpressLoader extends AbstractLoader {
         app.use(express.static(clientPath, options.serveStaticOptions));
         app.get(options.renderPath, renderFn);
       }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      app.use((err: any, _req: any, _res: any, _next: Function) => {
+        if (err instanceof HttpException) {
+          throw err;
+        } else if (err?.message?.includes('ENOENT') || err?.code === 'ENOENT') {
+          throw new NotFoundException(err.message);
+        }
+      });
     });
   }
 }

tests/e2e/express-adapter.e2e-spec.ts
@@ -1,0 +1,130 @@
+import { INestApplication } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Server } from 'net';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { NoopLogger } from '../utils/noop-logger';
+
+describe('Express adapter', () => {
+  let server: Server;
+  let app: INestApplication;
+
+  describe('when "fallthrough" option is set to "true"', () => {
+    beforeAll(async () => {
+      app = await NestFactory.create(AppModule.withFallthrough(), {
+        logger: new NoopLogger()
+      });
+      app.setGlobalPrefix('api');
+
+      server = app.getHttpServer();
+      await app.init();
+    });
+
+    describe('GET /api', () => {
+      it('should return "Hello, world!"', async () => {
+        return request(server).get('/api').expect(200).expect('Hello, world!');
+      });
+    });
+
+    describe('GET /', () => {
+      it('should return HTML file', async () => {
+        return request(server)
+          .get('/')
+          .expect(200)
+          .expect('Content-Type', /html/);
+      });
+    });
+
+    describe('GET /index.html', () => {
+      it('should return index page', async () => {
+        return request(server)
+          .get('/index.html')
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .expect(/Static website/);
+      });
+    });
+
+    describe('GET /logo.svg', () => {
+      it('should return logo', async () => {
+        return request(server)
+          .get('/logo.svg')
+          .expect(200)
+          .expect('Content-Type', /image/);
+      });
+    });
+
+    describe('when trying to get a non-existing file', () => {
+      it('should return index page', async () => {
+        return request(server)
+          .get('/404')
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .expect(/Static website/);
+      });
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+  });
+
+  describe('when "fallthrough" option is set to "false"', () => {
+    beforeAll(async () => {
+      app = await NestFactory.create(AppModule.withoutFallthrough(), {
+        logger: new NoopLogger()
+      });
+      app.setGlobalPrefix('api');
+
+      server = app.getHttpServer();
+      await app.init();
+    });
+
+    describe('GET /api', () => {
+      it('should return "Hello, world!"', async () => {
+        return request(server).get('/api').expect(200).expect('Hello, world!');
+      });
+    });
+
+    describe('GET /', () => {
+      it('should return HTML file', async () => {
+        return request(server)
+          .get('/')
+          .expect(200)
+          .expect('Content-Type', /html/);
+      });
+    });
+
+    describe('GET /index.html', () => {
+      it('should return index page', async () => {
+        return request(server)
+          .get('/index.html')
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .expect(/Static website/);
+      });
+    });
+
+    describe('GET /logo.svg', () => {
+      it('should return logo', async () => {
+        return request(server)
+          .get('/logo.svg')
+          .expect(200)
+          .expect('Content-Type', /image/);
+      });
+    });
+
+    describe('when trying to get a non-existing file', () => {
+      it('should return 404', async () => {
+        return request(server)
+          .get('/404')
+          .expect(404)
+          .expect(/Not Found/);
+      });
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+  });
+});

.circleci/config.yml
@@ -17,7 +17,7 @@ jobs:
   build:
     working_directory: ~/nest
     docker:
-      - image: cimg/node:21.7
+      - image: cimg/node:22.3
     steps:
       - checkout
       - restore_cache:
@@ -33,8 +33,23 @@ jobs:
           name: Build
           command: npm run build
 
+  e2e_test:
+    working_directory: ~/nest
+    docker:
+      - image: cimg/node:22.3
+    steps:
+      - checkout
+      - *restore-cache
+      - *install-deps
+      - run:
+          name: E2E tests
+          command: npm run test:e2e
+
 workflows:
   version: 2
   build-and-test:
     jobs:
       - build
+      - e2e_test:
+          requires:
+            - build

lib/interfaces/serve-static-options.interface.ts
@@ -7,7 +7,9 @@ export interface ServeStaticModuleOptions {
    */
   rootPath?: string;
   /**
-   * Path to render static app (concatenated with the `serveRoot` value). Default: * (wildcard - all paths). Note: `RegExp` is not supported by the `@nestjs/platform-fastify`.
+   * Path to render static app (concatenated with the `serveRoot` value).
+   * Default (express): {*any} (wildcard - all paths).
+   * Default (fastify): * (wildcard - all paths).
    */
   renderPath?: string | RegExp;
   /**
@@ -97,6 +99,25 @@ export interface ServeStaticModuleOptions {
      * stat the stat object of the file that is being sent
      */
     setHeaders?: (res: any, path: string, stat: any) => any;
+
+    /**
+     * Only for Fastify.
+     *
+     * When enabled, the server will attempt to send the Brotli encoded asset first,
+     * if supported by the Accept-Encoding headers. If Brotli is not supported,
+     * it will retry with gzip, and finally fall back to the original file.
+     *
+     * You may choose to skip compression for smaller files that do not benefit from it.
+     */
+    preCompressed?: boolean;
+
+    /**
+     * Only for Fastify.
+     *
+     * When false, reply object won't be automatically decorated with the sendFile method.
+     * @default true
+     */
+    decorateReply?: boolean;
   };
 }
 

lib/loaders/fastify.loader.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { loadPackage } from '@nestjs/common/utils/load-package.util';
 import { AbstractHttpAdapter } from '@nestjs/core';
 import * as fs from 'fs';
 import { ServeStaticModuleOptions } from '../interfaces/serve-static-options.interface';
 import {
-  DEFAULT_RENDER_PATH,
+  DEFAULT_FASTIFY_RENDER_PATH,
   DEFAULT_ROOT_PATH
 } from '../serve-static.constants';
 import { validatePath } from '../utils/validate-path.util';
@@ -24,10 +24,26 @@ export class FastifyLoader extends AbstractLoader {
     );
 
     optionsArr.forEach((options) => {
-      options.renderPath = options.renderPath || DEFAULT_RENDER_PATH;
+      options.renderPath = options.renderPath ?? DEFAULT_FASTIFY_RENDER_PATH;
 
-      const clientPath = options.rootPath || DEFAULT_ROOT_PATH;
+      const clientPath = options.rootPath ?? DEFAULT_ROOT_PATH;
       const indexFilePath = this.getIndexFilePath(clientPath);
+
+      const renderFn = (req: any, res: any) => {
+        fs.stat(indexFilePath, (err) => {
+          if (err) {
+            const stream = fs.createReadStream(indexFilePath);
+            if (options.serveStaticOptions?.setHeaders) {
+              const stat = fs.statSync(indexFilePath);
+              options.serveStaticOptions.setHeaders(res, indexFilePath, stat);
+            }
+            res.type('text/html').send(stream);
+          } else {
+            const error = new NotFoundException();
+            res.status(error.getStatus()).send(error.getResponse());
+          }
+        });
+      };
 
       if (options.serveRoot) {
         app.register(fastifyStatic, {
@@ -42,27 +58,14 @@ export class FastifyLoader extends AbstractLoader {
             ? options.serveRoot + validatePath(options.renderPath as string)
             : options.serveRoot;
 
-        app.get(renderPath, (req: any, res: any) => {
-          const stream = fs.createReadStream(indexFilePath);
-          res.type('text/html').send(stream);
-        });
+        app.get(renderPath, renderFn);
       } else {
         app.register(fastifyStatic, {
           root: clientPath,
           ...(options.serveStaticOptions || {}),
           wildcard: false
         });
-        app.get(options.renderPath, (req: any, res: any) => {
-          const stream = fs.createReadStream(indexFilePath);
-          if (
-            options.serveStaticOptions &&
-            options.serveStaticOptions.setHeaders
-          ) {
-            const stat = fs.statSync(indexFilePath);
-            options.serveStaticOptions.setHeaders(res, indexFilePath, stat);
-          }
-          res.type('text/html').send(stream);
-        });
+        app.get(options.renderPath, renderFn);
       }
     });
   }

lib/serve-static.constants.ts
@@ -1,4 +1,5 @@
 export const SERVE_STATIC_MODULE_OPTIONS = 'SERVE_STATIC_MODULE_OPTIONS';
 
 export const DEFAULT_ROOT_PATH = 'client';
-export const DEFAULT_RENDER_PATH = '*';
+export const DEFAULT_EXPRESS_RENDER_PATH = '{*any}';
+export const DEFAULT_FASTIFY_RENDER_PATH = '*';

lib/serve-static.module.ts
@@ -21,7 +21,7 @@ import { serveStaticProviders } from './serve-static.providers';
 export class ServeStaticModule implements OnModuleInit {
   constructor(
     @Inject(SERVE_STATIC_MODULE_OPTIONS)
-    private readonly ngOptions: ServeStaticModuleOptions[],
+    private readonly moduleOptions: ServeStaticModuleOptions[],
     private readonly loader: AbstractLoader,
     private readonly httpAdapterHost: HttpAdapterHost
   ) {}
@@ -85,8 +85,8 @@ export class ServeStaticModule implements OnModuleInit {
     };
   }
 
-  public async onModuleInit() {
+  public onModuleInit() {
     const httpAdapter = this.httpAdapterHost.httpAdapter;
-    this.loader.register(httpAdapter, this.ngOptions);
+    this.loader.register(httpAdapter, this.moduleOptions);
   }
 }

tests/client/index.html
@@ -1,0 +1,7 @@
+<img src="/logo.svg" width="100" />
+<h1>Static website</h1>
+<ul>
+  <li><a href="/link-1">Link 1</a></li>
+  <li><a href="/link-2">Link 2</a></li>
+  <li><a href="/link-3">Link 3</a></li>
+</ul>

commit 1aa5d8dd9bd844ea94d2985ad350e9a6c58b1709

fix: pass unhandled error on to next express error handler
Commit 0ae6f5415a81e4ea7daab65650f4015f986afedb introduced an error handling middleware.
However, it does not delegate to the next error handler if it cannot handle the error adequately.
This causes the request to hang until a timeout kills the connection.
By passing the error on to next, other error handlers (like the default error handler) handle it.

lib/loaders/express.loader.ts
@@ -83,6 +83,8 @@ export class ExpressLoader extends AbstractLoader {
           throw new NotFoundException(err.message);
         } else if (err?.code === 'ENOENT') {
           throw new NotFoundException(`ENOENT: ${err.message}`);
+        } else {
+          next(err);
         }
       });
     });

tests/e2e/express-adapter.e2e-spec.ts
@@ -9,6 +9,24 @@ describe('Express adapter', () => {
   let server: Server;
   let app: INestApplication;
 
+  describe('when middleware throws generic error', () => {
+    beforeAll(async () => {
+      app = await NestFactory.create(AppModule.withDefaults(), {
+        logger: new NoopLogger()
+      });
+      app.use((_req, _res, next) => next(new Error('Something went wrong')));
+
+      server = app.getHttpServer();
+      await app.init();
+    });
+
+    describe('GET /index.html', () => {
+      it('should return Iternal Server Error', async () => {
+        return request(server).get('/index.html').expect(500);
+      });
+    });
+  });
+
   describe('when "fallthrough" option is set to "true"', () => {
     beforeAll(async () => {
       app = await NestFactory.create(AppModule.withFallthrough(), {